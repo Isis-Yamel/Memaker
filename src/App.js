e7afab55080205c7ef9b31e95cef2fa1c06e6d04
Isis-Yamel <yamelamanca@gmail.com>
2022-10-14 10:56:37 -0500

    boostrap app with create react app and remove boilerplate

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header>
+        <p>
+          Boostrap app
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
